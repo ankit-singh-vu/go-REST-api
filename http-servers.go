01ee4743f098223c613bfae7ef3993322afa0050
ankit-singh-vu <ankisinggggg@gmail.com>
2024-01-02 00:09:50 +0530

    update http-servers.go and readme.md get req

--- http-servers.go
+++ http-servers.go
@@ -2,14 +2,50 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request){
-fmt.Fprint(w, "hello world")
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "hello world")
 }
 
-func main(){
-	http.HandleFunc("/hello",hello);
-	http.ListenAndServe(":3000", nil);
+func typicode_posts(w http.ResponseWriter, req *http.Request) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	response := resp
+	// fmt.Println("Response status:", resp.Status)
+	// fmt.Fprint(w, resp.Body)
+	// scanner := bufio.NewScanner(resp.Body)
+	// for i := 0; scanner.Scan() && i < 5; i++ {
+	// 	// fmt.Println()
+	// 	fmt.Fprint(w, scanner.Text())
+	// }
+
+	// if err := scanner.Err(); err != nil {
+	// 	panic(err)
+	// }
+	
+
+	// Set the Content-Type header to display JSON in the browser
+	w.Header().Set("Content-Type", "application/json")
+
+	// Copy the response body directly to the browser
+	_, err = io.Copy(w, response.Body)
+	if err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+		return
+	}
+
+
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+
+	http.HandleFunc("/typicode_get_posts", typicode_posts)
+	http.ListenAndServe(":3000", nil)
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 Rest api with go
+
+
+
+
+To do with gin frameework : https://gin-gonic.com/docs/quickstart/
